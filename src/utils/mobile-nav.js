5230327e7d44cbc0f0f0994d9d05bff5b6551765
kishan kumar <ftd10822422@dseu.ac.in>
2024-06-28 12:00:07 +0530

    first commit

---
+++ src/utils/mobile-nav.js
@@ -0,0 +1,31 @@
+const mobileNav = () => {
+    const headerMenuBtn = document.querySelector('.header__bars')
+    const mobileNav = document.querySelector('.mobile-nav')
+    const mobileMenuToggle = document.querySelector('.mobile-nav__toggle')
+    const mobileLinks = document.querySelectorAll('.mobile-nav__link')
+    
+    headerMenuBtn.addEventListener("click" , () => {
+        mobileNav.style.display = 'flex'
+        document.body.style.overflowY = 'hidden'
+        headerMenuBtn.classList.add('hide-Display')
+        mobileMenuToggle.classList.add('show-Display')
+    })
+
+    mobileMenuToggle.addEventListener("click", () => {
+        mobileNav.style.display = 'none'
+        headerMenuBtn.classList.remove('hide-Display')
+        mobileMenuToggle.classList.remove('show-Display')
+    })
+
+    mobileLinks.forEach(link => {
+        link.addEventListener("click" , () => {
+            mobileNav.style.display = 'none'
+            document.body.style.overflowY = 'auto'
+            headerMenuBtn.classList.remove('hide-Display')
+            mobileMenuToggle.classList.remove('show-Display')
+        })
+    })
+
+}
+
+export default mobileNav;
